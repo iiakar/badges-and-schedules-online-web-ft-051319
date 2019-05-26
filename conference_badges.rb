76ae572c39186b5dae40374c27930d2690543bdd
Ibrahim  Akar <ibsakar@gmail.com>
2019-05-26 03:27:30 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -7,9 +7,12 @@ def badge_maker(name)
 end 
 
 def batch_badge_creator(names)
+  
   badges=[]
+  
   names.each do |name|
-  badges << badge_maker(name)
+    badges << badge_maker(name)
+  end
+  
   return badges
-end
 end 
